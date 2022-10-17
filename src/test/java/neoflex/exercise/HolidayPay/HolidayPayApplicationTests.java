4c17bd7c0675a0a6a4671e484c6cdaa84451f00b
Mihail <mihail@git.git>
2022-10-17 19:01:24 +0300

    initial commit

---
+++ src/test/java/neoflex/exercise/HolidayPay/HolidayPayApplicationTests.java
@@ -0,0 +1,13 @@
+package neoflex.exercise.HolidayPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
