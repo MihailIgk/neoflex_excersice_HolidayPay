c966eb7f972178f713eb88f3f2fd3e2ec5ae6f5f
Mihail <mihail@git.git>
2022-10-18 17:13:41 +0300

    HTTP GET requests to /calculacte are mapped to calculacte() method

--- src/main/java/neoflex/exercise/HolidayPay/HolidayPayApplication.java
+++ src/main/java/neoflex/exercise/HolidayPay/HolidayPayApplication.java
@@ -2,12 +2,21 @@ package neoflex.exercise.HolidayPay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HolidayPayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HolidayPayApplication.class, args);
+	}
+	
+	@GetMapping("/calculacte")
+	public double calculacte(@RequestParam(value = "middleSalary", defaultValue = "120000") double middleSalary) {
+		return middleSalary;
 	}
 
 }
